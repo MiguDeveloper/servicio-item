76d3e52bab0aa6a4e5ef237c1069b0314dd0e93a
MiguDeveloper <miguel.peru.seo@gmail.com>
2020-01-07 12:49:59 -0500

    habilitamos hystrix y cliente eureka

--- src/main/java/pe/tuna/servicioitem/ServicioItemApplication.java
+++ src/main/java/pe/tuna/servicioitem/ServicioItemApplication.java
@@ -2,11 +2,14 @@ package pe.tuna.servicioitem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-// Si en caso tuvieramos mas clientes tendriamos que utilizar la anotacion en plural
-@RibbonClient(name = "servicio-productos")
+// Al tener configurado Eureka ya no es necesario definir esta anotacion
+// @RibbonClient(name = "servicio-productos") Si en caso tuvieramos mas clientes tendriamos que utilizar la anotacion en plural
+@EnableCircuitBreaker // Habilitamos Hystrix
+@EnableEurekaClient // cuando tenemos configurado un eureka
 @EnableFeignClients
 @SpringBootApplication
 public class ServicioItemApplication {
--- src/main/java/pe/tuna/servicioitem/controllers/ItemController.java
+++ src/main/java/pe/tuna/servicioitem/controllers/ItemController.java
@@ -1,11 +1,13 @@
 package pe.tuna.servicioitem.controllers;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import pe.tuna.servicioitem.models.Item;
+import pe.tuna.servicioitem.models.Producto;
 import pe.tuna.servicioitem.models.services.IItemService;
 
 import java.util.List;
@@ -22,8 +24,24 @@ public class ItemController {
         return itemService.findAll();
     }
 
+    // Aqui probamos el error que generamos en el microservicio productos, con timeout tambien
+    @HystrixCommand(fallbackMethod = "metodoAlternativo")
     @GetMapping("/ver/{id}/cantidad/{cantidad}")
     public Item detalle(@PathVariable Long id, @PathVariable int cantidad) {
         return itemService.findById(id, cantidad);
+    }
+
+    public Item metodoAlternativo(Long id, int cantidad){
+        Item item = new Item();
+        item.setCantidad(cantidad);
+
+        Producto producto = new Producto();
+        producto.setId(id);
+        producto.setNombre("PRODUCTO DEFAULT");
+        producto.setPrecio(500.00);
+
+        item.setProducto(producto);
+
+        return item;
     }
 }
